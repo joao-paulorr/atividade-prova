f3d0505d91b8ebcd3449bfb4070eb495d2bc0fa0
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-19 18:31:21 +0000

    k

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-function nota() {
-    let nome = prompt("Qual seu nome?")
-    let nota1 = Number.parseFloat(prompt("Qual a primeira nota que você tirou?"))
-    let nota2 = Number.parseFloat(prompt("Qual a segunda nota?"))
-    let media = (nota1 + nota2) / 2
-    alert(`Bem vindo ${nome}!`)
+// function nota() {
+//     let nome = prompt("Qual seu nome?")
+//     let nota1 = Number.parseFloat(prompt("Qual a primeira nota que você tirou?"))
+//     let nota2 = Number.parseFloat(prompt("Qual a segunda nota?"))
+//     let media = (nota1 + nota2) / 2
+//     alert(`Bem vindo ${nome}!`)
 
-    if (media >= 7) {
-        alert(`Você foi aprovado`)
-    } else if (media >= 4 && media <= 6.9) {
-        alert(`Você está em conselho de classe`)
-    } else if (media < 4) {
-        alert(`Você foi reprovado!`)
-    }
+//     if (media >= 7) {
+//         alert(`Você foi aprovado`)
+//     } else if (media >= 4 && media <= 6.9) {
+//         alert(`Você está em conselho de classe`)
+//     } else if (media < 4) {
+//         alert(`Você foi reprovado!`)
+//     }
+// }
 
-
+function triangulo() {
+    let base = prompt("Qual o tamanho da base do triângulo?")
+    let altura = prompt("Qual o tamanho da altura do triângulo?")
+    let conta = (base * altura) / 2
+    let div = document.getElementById("area")
+    div.innerText = conta
 }
