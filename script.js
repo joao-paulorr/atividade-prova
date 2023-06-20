12324fe4009e854d31d1bdc14211379497c991a2
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-20 15:11:56 +0000

    a

--- script.js
+++ script.js
@@ -14,10 +14,40 @@
 //     }
 // }
 
-function triangulo() {
-    let base = prompt("Qual o tamanho da base do triângulo?")
-    let altura = prompt("Qual o tamanho da altura do triângulo?")
-    let conta = (base * altura) / 2
-    let div = document.getElementById("area")
-    div.innerText = conta
+// function triangulo() {
+//     let base = prompt("Qual o tamanho da base do triângulo?")
+//     let altura = prompt("Qual o tamanho da altura do triângulo?")
+//     let conta = (base * altura) / 2
+//     let div = document.getElementById("area")
+//     div.innerText = conta
+// }
+
+function carros() {
+    let car = document.getElementById("carro").value
+    let pre = document.getElementById("preco")
+
+    switch (car) {
+        case "Gol 1.6":
+            pre.innerText = "R$48.000,00"
+            break;
+        
+        case "Saveiro 1.8":
+            pre.innerText = "R$56.500,00"
+            break;
+
+        case "Fiat Toro":
+            pre.innerText = "R$87.390,00"
+            break;
+
+        case "Susuki Vitara":
+            pre.innerText = "R$169.880,00"
+            break;
+
+        case "Corolla":
+            pre.innerText = "R$77.360,00"
+            break;
+        default:
+            pre.innerText = "Carro Inválido!"
+            break;
+    }
 }
