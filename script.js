e053be2c4bf5b869ceb7fcc57b63617a99dc455b
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-20 17:41:34 +0000

    k

--- script.js
+++ script.js
@@ -22,32 +22,38 @@
 //     div.innerText = conta
 // }
 
-function carros() {
-    let car = document.getElementById("carro").value
-    let pre = document.getElementById("preco")
+// function carros() {
+//     let car = document.getElementById("carro").value
+//     let pre = document.getElementById("preco")
 
-    switch (car) {
-        case "Gol 1.6":
-            pre.innerText = "R$48.000,00"
-            break;
+//     switch (car) {
+//         case "Gol 1.6":
+//             pre.innerText = "R$48.000,00"
+//             break;
         
-        case "Saveiro 1.8":
-            pre.innerText = "R$56.500,00"
-            break;
+//         case "Saveiro 1.8":
+//             pre.innerText = "R$56.500,00"
+//             break;
 
-        case "Fiat Toro":
-            pre.innerText = "R$87.390,00"
-            break;
+//         case "Fiat Toro":
+//             pre.innerText = "R$87.390,00"
+//             break;
 
-        case "Susuki Vitara":
-            pre.innerText = "R$169.880,00"
-            break;
+//         case "Susuki Vitara":
+//             pre.innerText = "R$169.880,00"
+//             break;
 
-        case "Corolla":
-            pre.innerText = "R$77.360,00"
-            break;
-        default:
-            pre.innerText = "Carro Inválido!"
-            break;
-    }
+//         case "Corolla":
+//             pre.innerText = "R$77.360,00"
+//             break;
+//         default:
+//             pre.innerText = "Carro Inválido!"
+//             break;
+//     }
+// }
+
+
+function cor(color) {
+let corpo = document.getElementsByTagName("body")[0]
+corpo.style.backgroundColor = color
 }
