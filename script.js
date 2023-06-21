5c2426df8b99f7ba71fa395fc6c3cd20c7a15baf
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-21 17:22:31 +0000

    f

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
-// // function nota() {
-// //     let nome = prompt("Qual seu nome?")
-// //     let nota1 = Number.parseFloat(prompt("Qual a primeira nota que você tirou?"))
-// //     let nota2 = Number.parseFloat(prompt("Qual a segunda nota?"))
-// //     let media = (nota1 + nota2) / 2
-// //     alert(`Bem vindo ${nome}!`)
+// // // function nota() {
+// // //     let nome = prompt("Qual seu nome?")
+// // //     let nota1 = Number.parseFloat(prompt("Qual a primeira nota que você tirou?"))
+// // //     let nota2 = Number.parseFloat(prompt("Qual a segunda nota?"))
+// // //     let media = (nota1 + nota2) / 2
+// // //     alert(`Bem vindo ${nome}!`)
 
-// //     if (media >= 7) {
-// //         alert(`Você foi aprovado`)
-// //     } else if (media >= 4 && media <= 6.9) {
-// //         alert(`Você está em conselho de classe`)
-// //     } else if (media < 4) {
-// //         alert(`Você foi reprovado!`)
-// //     }
-// // }
+// // //     if (media >= 7) {
+// // //         alert(`Você foi aprovado`)
+// // //     } else if (media >= 4 && media <= 6.9) {
+// // //         alert(`Você está em conselho de classe`)
+// // //     } else if (media < 4) {
+// // //         alert(`Você foi reprovado!`)
+// // //     }
+// // // }
 
-// // function triangulo() {
-// //     let base = prompt("Qual o tamanho da base do triângulo?")
-// //     let altura = prompt("Qual o tamanho da altura do triângulo?")
-// //     let conta = (base * altura) / 2
-// //     let div = document.getElementById("area")
-// //     div.innerText = conta
-// // }
+// // // function triangulo() {
+// // //     let base = prompt("Qual o tamanho da base do triângulo?")
+// // //     let altura = prompt("Qual o tamanho da altura do triângulo?")
+// // //     let conta = (base * altura) / 2
+// // //     let div = document.getElementById("area")
+// // //     div.innerText = conta
+// // // }
 
-// // function carros() {
-// //     let car = document.getElementById("carro").value
-// //     let pre = document.getElementById("preco")
+// // // function carros() {
+// // //     let car = document.getElementById("carro").value
+// // //     let pre = document.getElementById("preco")
 
-// //     switch (car) {
-// //         case "Gol 1.6":
-// //             pre.innerText = "R$48.000,00"
-// //             break;
+// // //     switch (car) {
+// // //         case "Gol 1.6":
+// // //             pre.innerText = "R$48.000,00"
+// // //             break;
         
-// //         case "Saveiro 1.8":
-// //             pre.innerText = "R$56.500,00"
-// //             break;
+// // //         case "Saveiro 1.8":
+// // //             pre.innerText = "R$56.500,00"
+// // //             break;
 
-// //         case "Fiat Toro":
-// //             pre.innerText = "R$87.390,00"
-// //             break;
+// // //         case "Fiat Toro":
+// // //             pre.innerText = "R$87.390,00"
+// // //             break;
 
-// //         case "Susuki Vitara":
-// //             pre.innerText = "R$169.880,00"
-// //             break;
+// // //         case "Susuki Vitara":
+// // //             pre.innerText = "R$169.880,00"
+// // //             break;
 
-// //         case "Corolla":
-// //             pre.innerText = "R$77.360,00"
-// //             break;
-// //         default:
-// //             pre.innerText = "Carro Inválido!"
-// //             break;
-// //     }
+// // //         case "Corolla":
+// // //             pre.innerText = "R$77.360,00"
+// // //             break;
+// // //         default:
+// // //             pre.innerText = "Carro Inválido!"
+// // //             break;
+// // //     }
+// // // }
+
+
+// // function cor(color) {
+// // let corpo = document.getElementsByTagName("body")[0]
+// // corpo.style.backgroundColor = color
 // // }
 
+// function ano() {
+//     let anos = document.getElementById("ano").value
+//     let meses = document.getElementById("mes").value
+//     let dias = document.getElementById("dia").value
+//     let nascimento = anos + meses + dias
+//     let calculoa = anos - 2023
+//     let calculom = meses - 12
+//     let calculod = dias - 365
 
-// function cor(color) {
-// let corpo = document.getElementsByTagName("body")[0]
-// corpo.style.backgroundColor = color
+//     alert(`Você já viveu ${calculoa} anos, ${calculom} e ${calculod}.`)
 // }
-
-function ano() {
-    let anos = document.getElementById("ano").value
-    let meses = document.getElementById("mes").value
-    let dias = document.getElementById("dia").value
-    let nascimento = anos + meses + dias
-    let calculoa = anos - 2023
-    let calculom = meses - 12
-    let calculod = dias - 365
-
-    alert(`Você já viveu ${calculoa} anos, ${calculom} e ${calculod}.`)
-}
